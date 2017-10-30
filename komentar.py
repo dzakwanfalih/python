b618ed724814661f8fc46dd147ac430c7dc8407f
dzakwanfalih <dzakwanfalih@hotmail.com>
2017-10-30 13:11:01 +0700

    add comment tutorial

---
+++ komentar.py
@@ -0,0 +1,13 @@
+#############################################################################
+#									    #	
+# 			Supernode Learning Python 2.7			    #
+#									    #
+#############################################################################
+
+# Date : 30 Oktober 2017 
+
+# menulis Pyton dengan tanda pagar 
+
+print ("Menulis python ") # init merupakan perintah print
+print (123) # cara menulis angka / integer
+
